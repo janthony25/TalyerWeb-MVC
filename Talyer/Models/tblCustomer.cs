2d50236e9eea2535120797947d87b3d7f3b9570e
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-16 18:43:03 +1300

    Add tblCustomerModel

---
+++ Talyer/Models/tblCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talyer.Models
+{
+    public class tblCustomer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string CarRego { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string AddedBy { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
